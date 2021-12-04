46c9e65f744c2d7105d0e7a795d3023728931932
IvanSmiths <info@ivansmiths.com>
2021-12-04 09:50:54 +0100

    ADD Animation

--- components/HomePage/About.js
+++ components/HomePage/About.js
@@ -1,86 +1,506 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { motion } from 'framer-motion';
 import ImageComponent from '../ImageComponent';
 const About = () => {
   return (
     <main className="main-cnt flex-center">
       <div className="flex-center">
-        <p className="medium-font">Lorem ipsum</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Lorem ipsum
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">consectetur adipiscing elit.</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          consectetur adipiscing elit.
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">Pellentesque nulla ipsum,</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Pellentesque nulla ipsum,
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
           Morbi dignissim, sit Curabitur felis nisl, rhoncus vel Lorem ipsum
           dolor sit amet,
-        </p>
-
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">consectetur adipiscing elit.</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          consectetur adipiscing elit.
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">Pellentesque nulla ipsum,</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Pellentesque nulla ipsum,
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">Pellentesque nulla ipsum,</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Pellentesque nulla ipsum,
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">Morbi dignissim, sit</p>
-        <ImageComponent
-          src={'/main-image.jpg'}
-          webp={'/main-image.webp'}
-          height={'100%'}
-          width={'160px'}
-          alt={'sito archeologico di san quirico, pozzo'}
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Morbi dignissim, sit
+        </motion.p>
+        <motion.picture
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
           className={'main-paragraph-image'}
-        />
-        <p className="medium-font">Curabitur felis nisl, rhoncus vel</p>
+        >
+          <source
+            alt={'sito archeologico di san quirico, pozzo'}
+            decoding={'async'}
+            loading={'lazy'}
+            height={'100%'}
+            width={'160px'}
+            srcSet={'/main-image.webp'}
+            type="image/webp"
+          />
+          <img
+            alt={'sito archeologico di san quirico, pozzo'}
+            loading={'lazy'}
+            decoding={'async'}
+            src={'/main-image.jpg'}
+            height={'100%'}
+            width={'160px'}
+          />
+        </motion.picture>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Curabitur felis nisl, rhoncus vel
+        </motion.p>
+      </div>
+      <div className="test-cnt">
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="medium-font"
+        >
+          Lorem ipsum{' '}
+          <motion.picture
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className={'main-paragraph-image'}
+          >
+            <source
+              alt={'sito archeologico di san quirico, pozzo'}
+              decoding={'async'}
+              loading={'lazy'}
+              height={'100%'}
+              width={'160px'}
+              srcSet={'/main-image.webp'}
+              type="image/webp"
+            />
+            <img
+              alt={'sito archeologico di san quirico, pozzo'}
+              loading={'lazy'}
+              decoding={'async'}
+              src={'/main-image.jpg'}
+              height={'100%'}
+              width={'160px'}
+            />
+          </motion.picture>{' '}
+          dolor, sit amet{' '}
+          <motion.picture
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className={'main-paragraph-image'}
+          >
+            <source
+              alt={'sito archeologico di san quirico, pozzo'}
+              decoding={'async'}
+              loading={'lazy'}
+              height={'100%'}
+              width={'160px'}
+              srcSet={'/main-image.webp'}
+              type="image/webp"
+            />
+            <img
+              alt={'sito archeologico di san quirico, pozzo'}
+              loading={'lazy'}
+              decoding={'async'}
+              src={'/main-image.jpg'}
+              height={'100%'}
+              width={'160px'}
+            />
+          </motion.picture>{' '}
+          consectetur adipisicing{' '}
+          <motion.picture
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className={'main-paragraph-image'}
+          >
+            <source
+              alt={'sito archeologico di san quirico, pozzo'}
+              decoding={'async'}
+              loading={'lazy'}
+              height={'100%'}
+              width={'160px'}
+              srcSet={'/main-image.webp'}
+              type="image/webp"
+            />
+            <img
+              alt={'sito archeologico di san quirico, pozzo'}
+              loading={'lazy'}
+              decoding={'async'}
+              src={'/main-image.jpg'}
+              height={'100%'}
+              width={'160px'}
+            />
+          </motion.picture>{' '}
+          elit. Mollitia totam nobis{' '}
+          <motion.picture
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className={'main-paragraph-image'}
+          >
+            <source
+              alt={'sito archeologico di san quirico, pozzo'}
+              decoding={'async'}
+              loading={'lazy'}
+              height={'100%'}
+              width={'160px'}
+              srcSet={'/main-image.webp'}
+              type="image/webp"
+            />
+            <motion.img
+              whileHover={{
+                scale: 1.2,
+                transition: { duration: 1 },
+              }}
+              whileTap={{ scale: 0.9 }}
+              alt={'sito archeologico di san quirico, pozzo'}
+              loading={'lazy'}
+              decoding={'async'}
+              src={'/main-image.jpg'}
+              height={'100%'}
+              width={'160px'}
+            />
+          </motion.picture>{' '}
+          officiis vitae itaque repudiandae perspiciatis praesentium. Minima,
+          non! Repudiandae provident saepe atque voluptate eveniet odio harum
+          vero eius in. Atque illo eius quibusdam harum, et cum? Repellat
+          provident ad explicabo, eius maiores praesentium rem blanditiis
+          aspernatur tempora dolorem sunt.
+        </motion.p>
       </div>
     </main>
   );
--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -4,13 +4,6 @@ import React from 'react';
 function SrcImage({ src, webp, alt, height, width, className, ...delegated }) {
   return (
     <picture className={className}>
-      {/* <source
-        alt={'dw'}
-        height={100}
-        width={100}
-        srcSet="/images/cloudflare-stats.avif"
-        type="image/avif"
-      /> */}
       <source
         alt={alt}
         decoding={'async'}
